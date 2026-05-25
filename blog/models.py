7d1cdca55f398c88dc0f8b5d49b7f77c055f9b29
alancoomeynts-hub <alancoomeynts@gmail.com>
2026-05-25 18:51:09 +0100

    Add Templates to blog and render Posts on generic view

--- blog/models.py
+++ blog/models.py
@@ -3,23 +3,25 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
+# Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(max_length=200,unique=True)
-    author=models.ForeignKey(
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    content=models.TextField()
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt=models.TextField(blank=True)    
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ["-created_on"]
 
     def __str__(self):
-        return f'{self.title} | written by {self.author}'
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     post=models.ForeignKey(
@@ -31,6 +33,7 @@ class Comment(models.Model):
     content=models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
 
     class Meta:
         ordering = ['created_on']
